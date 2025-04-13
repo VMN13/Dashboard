da0a362470f26e6c67d5384aa6cdcef7c87ede12
VMN13 <Stayspaceflux@mail.com>
2025-04-13 20:00:27 +0300

    webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-
-
+import './styles.css'
+import IMAGE from './image.png';
+import LOGO from './react.svg'
 export const App = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Dashboard - {process.env.NODE_ENV} {process.env.name}</h1>
+      <img src={IMAGE} alt="image" />
+      <img src={LOGO} alt="logo" width="100px" />
     </div>
   )
 }
