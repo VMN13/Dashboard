46997b15d82c8b3fe7a9f9691844c02ce6ddd731
VMN13 <Stayspaceflux@mail.com>
2025-04-14 07:53:39 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ClickCounter } from './ClickCounter';
 import './styles.css'
 import IMAGE from './image.png';
 import LOGO from './react.svg'
 export const App = () => {
-  return (
-  
+  return (    
     <div>
-      <h1>Dashboard - {process.env.NODE_ENV} {process.env.name}</h1>
-      <img src={IMAGE} alt="image" />
-      <img src={LOGO} alt="logo" width="100px" />
-      <ClickCounter />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ClickCounter />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
+
+
+
